cb98e4fca7ddeec1ae5da2575f11e662c8db958a
Maureen Roble <moroble@gmail.com>
2016-12-15 21:15:14 -0500

    Correct victory screen code

--- js/stateVictory.js
+++ js/stateVictory.js
@@ -5,18 +5,17 @@ var StateVictory = {
     },
 
     create: function () {
-        this.buttonPlayAgain = game.add.button(game.world.centerX, game.world.centerY + 100, "buttons", this.replay, this, 0, 1, 0);
-        this.buttonPlayAgain.anchor.set(0.5, 0.5);
-        
-         //dragon
+        this.buttonStart = game.add.button(game.world.centerX, game.world.centerY + 100, "buttons", this.startGame, this, 6, 7, 6);
+        this.buttonStart.anchor.set(0.5, 0.5);
+
+        //dragon
         this.dragon = game.add.sprite(game.world.centerX, game.world.centerY, "dragon");
-        this.dragon.anchor.set(0.5,0.5);
+        this.dragon.anchor.set(0.5, 0.5);
         this.dragon.animations.add('fly', [0, 1, 2, 3], 12, true);
         this.dragon.animations.play('fly');
-        this.dragon.scale.x=-1;
-        game.stage.backgroundColor="#45B5AA";
-        
-    this.titleText = game.add.text(game.world.centerX, 60, "You Win!", {
+        game.stage.backgroundColor = "#45B5AA";
+
+        this.titleText = game.add.text(game.world.centerX, 60, "Kalipay And The Tiniest Tiktik", {
             font: "50px Architects Daughter"
             , fill: "#ffcc00"
             , stroke: "#800000"
@@ -25,9 +24,7 @@ var StateVictory = {
         });
         this.titleText.anchor.set(0.5, 0.5);
     }
-
-
-    , replay: function () {
+    , startGame: function () {
         game.state.start("StateMain");
     }
     , update: function () {
