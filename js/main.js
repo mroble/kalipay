67598d3a37d90dd30190a1a890eb6232241efc3b
Maureen Roble <moroble@gmail.com>
2017-06-04 16:35:56 -0400

    clean up code for main.js

--- js/main.js
+++ js/main.js
@@ -3,15 +3,31 @@ var score;
 var soundOn = true;
 var musicOn = true;
 
+var useLandscape = true;
+
+
 window.onload = function () {
     if (screen.width > 1500) {
         //desktop laptop
-        game = new Phaser.Game(640, 480, Phaser.AUTO, "ph_game");
+        if (useLandscape == true) {
+            game = new Phaser.Game(640, 480, Phaser.AUTO, "ph_game");
+        } else {
+
+            game = new Phaser.Game(480, 640, Phaser.AUTO, "ph_game");
+        }
 
     } else {
         //mobile device
         game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "ph_game");
     }
+    if (screen.width < 1500) {
+        if (useLandscape == true) {
+            wrongTag = "wrongWayLandscape";
+        } else {
+            wrongTag = "wrongWayPortrait";
+        }
+    }
+
     //add a state or screen to the game
     game.state.add("StateMain", StateMain);
     game.state.add("StateTitle", StateTitle);
