c5f1f17575c9b10c8e455824e288c3505fc525ec
Maureen Roble <moroble@gmail.com>
2017-06-04 16:40:58 -0400

    Fix code in stateinit.js

--- js/stateInit.js
+++ js/stateInit.js
@@ -4,12 +4,17 @@ var StateInit = {
         game.load.image("loadingEmpty", "images/loading/progress_none.png");
         game.load.image("loadingFull", "images/loading/progress_all.png");
 
+        if (screen.width < 1500) {
+            if (useLandscape == true) {
+                game.scale.forceOrientation(true, false);
+            } else {
+                game.scale.forceOrientation(false, true);
+            }
 
-        game.scale.forceOrientation(true, false);
 
-
-        game.scale.enterIncorrectOrientation.add(this.wrongWay, this);
-        game.scale.leaveIncorrectOrientation.add(this.rightWay, this);
+            game.scale.enterIncorrectOrientation.add(this.wrongWay, this);
+            game.scale.leaveIncorrectOrientation.add(this.rightWay, this);
+        }
 
     }
     , create: function () {
@@ -19,10 +24,10 @@ var StateInit = {
 
     }
     , rightWay: function () {
-        document.getElementById("wrong").style.display = "none";
+        document.getElementById(wrongTag).style.display = "none";
     }
     , wrongWay: function () {
-        document.getElementById("wrong").style.display = "block";
+        document.getElementById(wrongTag).style.display = "block";
     }
 
 }
