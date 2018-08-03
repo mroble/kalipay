e08c972336fd21b22f3be1c70ffdc519c0706af0
Maureen Roble <moroble@gmail.com>
2018-08-02 21:20:53 -0400

    add end graphic

--- js/stateVictory.js
+++ js/stateVictory.js
@@ -9,16 +9,19 @@ var StateVictory = {
         this.buttonStart = game.add.button(game.world.centerX, game.world.centerY + 100, "buttons", this.startGame, this, 6, 7, 6);
         this.buttonStart.anchor.set(0.5, 0.5);
 
-        //gamay
-        this.gamay = game.add.sprite(game.world.centerX, game.world.centerY, "gamay");
-        this.gamay.anchor.set(0.5, 0.5);
-        this.gamay.animations.add('fly', [0, 1, 2, 3], 12, true);
-        this.gamay.animations.play('fly');
-        game.stage.backgroundColor = "#26C9FF";
+        var victory = game.add.image(game.world.centerX, game.world.centerY, "DidIt");
 
-        this.inText = game.add.text(game.world.centerX,30,"You did it!");
-        this.inText.fill="#000000";
-        this.inText.anchor.set(0.5, 0.5);
+
+        //gamay
+        //this.gamay = game.add.sprite(game.world.centerX, game.world.centerY, "gamay");
+        //this.gamay.anchor.set(0.5, 0.5);
+        //this.gamay.animations.add('fly', [0, 1, 2, 3], 12, true);
+        //this.gamay.animations.play('fly');
+        //game.stage.backgroundColor = "#26C9FF";
+
+        //this.inText = game.add.text(game.world.centerX,30,"You did it!");
+        //this.inText.fill="#000000";
+        //this.inText.anchor.set(0.5, 0.5);
 
          //audio
          //record a happy message later
