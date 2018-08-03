1fc1cd9fc1d045832af625f9ec8b5aa513b93ee8
Maureen Roble <moroble@gmail.com>
2018-08-02 22:01:25 -0400

    add new graphic

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -6,7 +6,7 @@ var StateTitle = {
 
     create: function () {
 
-        this.title=game.add.sprite(game.world.centerX-350, game.world.centerY - 300,"title");
+        this.title=game.add.sprite(game.world.centerX, game.world.centerY,"title");
         this.buttonStart = game.add.button(game.world.centerX - 20, game.world.centerY + 115, "buttons", this.startGame, this, 6, 7, 6);
         this.buttonStart.anchor.set(0.5, 0.5);
 
