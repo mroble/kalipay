fbe9cc1fb89865c48d9d3e94b07078601806a7a7
Maureen Roble <moroble@gmail.com>
2018-08-02 21:28:38 -0400

    add stuff

--- js/stateVictory.js
+++ js/stateVictory.js
@@ -9,7 +9,7 @@ var StateVictory = {
         this.buttonStart = game.add.button(game.world.centerX, game.world.centerY + 100, "buttons", this.startGame, this, 6, 7, 6);
         this.buttonStart.anchor.set(0.5, 0.5);
 
-        var victory = game.add.image(game.world.centerX, game.world.centerY, "DidIt");
+        var victory = this.add.image(game.world.centerX, game.world.centerY, "DidIt");
 
 
         //gamay
