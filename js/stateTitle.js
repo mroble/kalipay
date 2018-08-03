8574cef2d92d725c79d8aa5092d842212ad4cab9
Maureen Roble <moroble@gmail.com>
2018-08-02 22:55:42 -0400

    fix title graphic position

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -6,7 +6,12 @@ var StateTitle = {
 
     create: function () {
 
-        this.title=game.add.sprite(game.world.centerX, game.world.centerY,"title");
+        var title = this.add.image(0, 0, "title");
+        title.x = this.world.centerX;
+        title.y = this.world.centerY;
+        title.anchor.set(0.5, 0.5);
+
+
         this.buttonStart = game.add.button(game.world.centerX - 20, game.world.centerY + 115, "buttons", this.startGame, this, 6, 7, 6);
         this.buttonStart.anchor.set(0.5, 0.5);
 
