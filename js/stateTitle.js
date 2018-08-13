673ab958e2d19a4d214debc919442d9c230ef52f
Maureen Roble <moroble@gmail.com>
2018-08-12 22:46:24 -0400

    try again

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -12,7 +12,7 @@ var StateTitle = {
         title.anchor.set(0.5, 0.5);
 
 
-        this.buttonStart = game.add.button(game.world.centerX - 20, game.world.centerY + 155, "buttons", this.startGame, this, 6, 7, 6);
+        this.buttonStart = game.add.button(game.world.centerX - 20, game.world.centerY + 215, "buttons", this.startGame, this, 6, 7, 6);
         this.buttonStart.anchor.set(0.5, 0.5);
 
 
