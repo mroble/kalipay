d7b947d98e6cdd967855beac74bb21b10ac0b627
Maureen Roble <moroble@gmail.com>
2018-08-15 13:19:43 -0400

    add background music

--- js/stateLoad.js
+++ js/stateLoad.js
@@ -20,20 +20,20 @@ var StateLoad = {
         game.load.image("title", "images/ui/Kalipay_GameTitle.png");
         game.load.image("kamatisToast", "images/main/kamatis.png");
         game.load.image("manggaToast", "images/main/mangga.png");
-        game.load.image("balanghoyToast", "images/main/balanghoy.png");
+        game.load.image("petsayToast", "images/main/petsay.png");
         game.load.image("kalabasaToast", "images/main/kalabasa.png");
         game.load.spritesheet("veggie", "images/main/veggie.png", 52, 50, 4);
         game.load.image("balloon", "images/main/thought.png");
         game.load.spritesheet("soundButtons", "images/ui/soundButtons.png", 44, 44, 4);
 
         //change this song
-        game.load.audio("backgroundMusic", "sounds/song18.mp3");
-        game.load.audio("balanghoy", "sounds/food/balanghoy.wav");
-        game.load.audio("mangga", "sounds/food/mangga.wav");
-        game.load.audio("kamatis", "sounds/food/kamatis.wav");
-        game.load.audio("kalabasa", "sounds/food/kalabasa.wav");
-        game.load.audio("yummy", "sounds/food/yummy.wav");
-        game.load.audio("instructions", "sounds/gamay_instructions.wav");
+        game.load.audio("backgroundMusic", "sounds/naturesounds.mp3");
+        //game.load.audio("balanghoy", "sounds/food/balanghoy.wav");
+        //game.load.audio("mangga", "sounds/food/mangga.wav");
+        //game.load.audio("kamatis", "sounds/food/kamatis.wav");
+        //game.load.audio("kalabasa", "sounds/food/kalabasa.wav");
+        //game.load.audio("yummy", "sounds/food/yummy.wav");
+        //game.load.audio("instructions", "sounds/gamay_instructions.wav");
         game.load.spritesheet("buttons", "images/ui/buttons.png", 265, 75);
 
     },
